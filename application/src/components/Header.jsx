8e31039a5f62eb04048b96215e5450cc66ca1ee1
Ricardo <lrsj2003@gmail.com>
2022-09-29 01:29:38 -0300

    comeco do site

--- application/src/components/Header.jsx
+++ application/src/components/Header.jsx
@@ -1,9 +1,6 @@
-import styles from "../styles/Home.module.css"
-
-
 export default function Header(){
     return (
-    <div className={styles.h1}>
+    <div >
         <h1>Unb Na Grade</h1>
         <p >
             Pq gastar 30 minutos no sigaa se vc pode gastar 30 horas no vscode
