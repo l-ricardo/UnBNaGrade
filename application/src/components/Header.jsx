0ab57fc6673b4fcfcf7bf391aeac189a12fc0280
Ricardo <lrsj2003@gmail.com>
2022-09-29 04:25:55 -0300

    html base de header

--- application/src/components/Header.jsx
+++ application/src/components/Header.jsx
@@ -1,10 +1,9 @@
+import styles from "../styles/Header.module.css";
+
 export default function Header(){
     return (
-    <div >
-        <h1>Unb Na Grade</h1>
-        <p >
-            Pq gastar 30 minutos no sigaa se vc pode gastar 30 horas no vscode
-        </p>
-    </div>
+    <h1 className={styles.faixaTopo}>
+        UNB NA GRADE
+    </h1>
     )
 }
