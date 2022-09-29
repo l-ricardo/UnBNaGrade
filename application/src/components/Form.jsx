280e641b072818540058b9efb1ebe6d06a814e2d
Ricardo <lrsj2003@gmail.com>
2022-09-29 04:25:23 -0300

    html base de form

--- application/src/components/Form.jsx
+++ application/src/components/Form.jsx
@@ -1,10 +1,10 @@
+import styles from "../styles/Form.module.css";
+
 export default function Form(){
     return (
-    <div>
-        <h1>Form</h1>
-        <p >
-            Contem o formulario onde entraram as opções de disciplinas a se inscrever
-        </p>
+    <div className={styles.caixote}>
+        <label>Insira o codigo de uma diciplina que deseja adicionar</label>
+        <input type="text" />
     </div>
     )
 }
