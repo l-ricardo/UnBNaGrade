696b5e9261ccd5d3a9a818e65980527ab5b55b2f
Ricardo <lrsj2003@gmail.com>
2022-09-29 01:45:20 -0300

    removendo estilos inuteis

--- application/src/pages/_app.jsx
+++ application/src/pages/_app.jsx
@@ -1,6 +1,4 @@
-// import '../styles/global.css'
-
-import '../styles/global.css'
+import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
