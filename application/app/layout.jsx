41b83a34182fbb93bf633c7d6235303a2c42e69a
Ricardo <lrsj2003@gmail.com>
2023-08-02 00:12:58 -0300

    prettier

--- application/app/layout.jsx
+++ application/app/layout.jsx
@@ -1,17 +1,17 @@
-import '../styles/globals.css'
-import { Inter } from 'next/font/google'
+import "../styles/globals.css"
+import { Inter } from "next/font/google"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: 'UnBNaGrade',
-  description: 'Te ajudando a montar sua grade horária',
+    title: "UnBNaGrade",
+    description: "Te ajudando a montar sua grade horária",
 }
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="pt-br">
+            <body className={inter.className}>{children}</body>
+        </html>
+    )
 }
--- application/components/Dropdown.jsx
+++ application/components/Dropdown.jsx
@@ -2,9 +2,9 @@
 // import makeAnimated from "react-select/animated"
 // import disciplinas from '../../public/data/disciplinas.json';
 
-
-export default function CaixaSelecao(){
-    return (<p>select</p>
+export default function CaixaSelecao() {
+    return (
+        <p>select</p>
         // <Select
         //     // defaultValue={} // TODO: Sicronizar valores default com o local storage para evitar que quando a pagina for recarregada o dropdown esta vazio mas o resto da pagina ainda tem conteudo. (Bug menor)
         //     components={makeAnimated()} // TODO: Esta causando um bug visual no primeiro item selecionado
